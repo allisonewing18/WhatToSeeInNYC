3196f9303bf244a2b86f1cdc3e70aba25714163a
Allison Ewing <ewinga@cua.edu>
2025-10-08 19:19:51 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-console.log("Hello World");
-const a = 0.1 + 0.2
-const b = 0.3
-console.log(a === b);
-
-if (true) {
-    let x = 15;
-    const y = 20;
-}
-console.log(x);
-console.log(y);
-
 const firstName = "Allison";
 const lastName = "Ewing;"
 
