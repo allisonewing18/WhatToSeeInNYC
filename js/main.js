87c654555180fb6e8bbdf8593fc387091e460a3a
Allison Ewing <ewinga@cua.edu>
2025-10-08 19:23:13 -0400

    updated practice

--- js/main.js
+++ js/main.js
@@ -2,27 +2,32 @@ const firstName = "Allison";
 const lastName = "Ewing;"
 
 function fullName(firstName, lastName) {
-    return fullName = firstName + " " + lastName;
+    return fullname = firstName + " " + lastName;
 }
-console.log(fullName);
+console.log(fullname);
 
 const array = [1, 2, 3, 4, 5];
 let count = 0;
-function mean(array) 
+function mean(array)
 {
     for (let i =0; i<array.length;i++) 
     {
         count+= array[i];
     }
-    return mean = count/array.length;
+    return Mean = count/array.length;
 }
+console.log(Mean);
 
 let integer = Math.random();
 function numberprint(integer) 
 {
     for (let i =1; i<=integer; i++)
     {
-        console.log(" ")
+        console.log(" ");
+        for (let j=1;j<=integer;j++)
+        {
+            console.log(j);
+        }
     }
 }
 console.log(numberprint);
