46a8bfa7982b8a0e7bb5338e6f020f4146766903
Allison Ewing <ewinga@cua.edu>
2025-10-15 18:34:07 -0400

    added data js file

---
+++ js/data.js
@@ -0,0 +1,94 @@
+const data = 
+{
+    sections: 
+    [
+        {
+            uniqueId: "header",
+            title: "What To See in NYC",
+            articles:
+            [
+                {
+                    title: "What To See in NYC",
+                    text: "Your go-to guide for what is worth your precious time and money spent in The City That Never Sleeps!",
+                }
+            ]
+        },
+
+        {
+            uniqueId: "section 1",
+            title: "Must Sees",
+            articles: 
+            [
+                {
+                    title: "Wicked",
+                    text: "98% Positive Reviews Reviews: A Broadway must that has only grown in popularity over the recent movie adaptations. Wicked remains a classic story and entertaining show that spins everything audiences think they know about the land of Oz. Audiences love the wide variety of songs, powerful vocals, and dazzling costumes & coreography.",
+                    imgSrc: "images/wicked.png",
+                    alt: "Wicked Playbill"
+                },
+                {
+                    title: "Hamilton",
+                    text: "98% Positive Reviews Reviews: One of the most iconic and recognizable musicals of the 21st century! Lin Manuel-Miranda's third major Broadway musical is still selling out 10 years after its first performance! Audiences love the modern, hip-hop retelling of the stories about our nation's founding fathers with dazzling coreography throughout!",
+                    imgSrc: "images/hamilton.jpg",
+                    alt: "Hamilton Playbill"
+                },
+                {
+                    title: "Maybe Happy Ending",
+                    text: "97% Positive Reviews Reviews: Audiences rave over the new, whimsical musical that earned it's Best Musical Tony. The simple yet heartwarming plot, accompanied by impressive robotic acting, makes Maybe Happy Ending a pleasant show with a love story that will restore your faith in humanity.",
+                    imgSrc: "images/mhe.png",
+                    alt: "Maybe Happy Ending Playbill"
+                },
+            ]
+        },
+        {
+            uniqueId: "section 2",
+            title: "Mostly Positive",
+            articles: 
+            [
+                {
+                    title: "Operation Mincemeat",
+                    text: "90% Positive Reviews Reviews: A hilarious British Musical that sheds light on such an important mission in WW2! Although some audiences find the British accents hard to understand at times, this show is jam packed with humor. With constant costume changes and impressive prop/scene work, you are always on the edge of your seat and laughing from beginning to end! Even while being such a powerful comedy, Operation Mincemeat knows how to balance the mood with a few powerful ballads and right the moral wrongs of the original operation.",
+                    imgSrc: "images/opmincemeat.png",
+                    alt: "Operation Mincemeat Playbill"
+                },
+                {
+                    title: "Death Becomes Her",
+                    text: "94% Positive Reviews Reviews: This musical is a movie adaptation done right! It's humor, wit, and special effects make it a good time! Some audiences find the story a bit lack-luster and the music forgettable, but the performances of the two leading ladies makes it a fun and laugh-filled experience for any audience member!",
+                    imgSrc: "images/dbh.png",
+                    alt: "Death Becomes Her Playbill"
+                },
+        
+            ]
+        },
+        {
+            uniqueId: "section 3",
+            title: "Mixed Reviews",
+            articles: 
+            [
+                {
+                    title: "Cabaret",
+                    text: "90% Positive Reviews Reviews: In the beginning of their run, reviews were fantastic for this long-awaited arrival. However, reviews have dropped since Billy Porter joined the cast. Now at the time of writing, Cabaret is now closed due to this recent turn in reviews and attendence.",
+                    imgSrc: "images/cabaret.png",
+                    alt: "Cabaret Playbill"
+                },
+                {
+                    title: "Moulin Rouge",
+                    text: "87% Positive Reviews Reviews: Jukebox musicals are always tough to pull off, but Moulin Rougue makes a good effort at puttig on a fun spectacle full of songs everyone will know! Many criticize it's overly dramatic nature with a low stakes plot, but any Millenial or Gen Z audience member will know nearly every song! Although turning Katy Perry's Firework into a power ballad doesn't quite land, the remixes of songs like Crazy & Rolling in the Deep and Shut Up and Dance & Raise Your Glass with impressive coreography make this show a good time from curtain rise to fall!",
+                    imgSrc: "images/mr.png",
+                    alt: "Moulin Rouge Playbill"
+                },
+        
+            ]
+        },
+        {
+            uniqueId: "footer",
+            title: "footer",
+            articles:
+            [
+                {
+                    text: "Reviews via New York Theatre Guide before being reworded and consolidated by Allison Ewing Questions? Email me at ewinga@cua.edu",
+                }
+            ]
+        }
+
+    ]
+}
--- js/main.js
+++ js/main.js
@@ -1,33 +1,11 @@
-const firstName = "Allison";
-const lastName = "Ewing;"
-
-function fullName(firstName, lastName) {
-    return fullname = firstName + " " + lastName;
-}
-console.log(fullname);
-
-const array = [1, 2, 3, 4, 5];
-let count = 0;
-function mean(array)
+const user = 
 {
-    for (let i =0; i<array.length;i++) 
-    {
-        count+= array[i];
-    }
-    return Mean = count/array.length;
-}
-console.log(Mean);
+    firstName: "Allison",
+    lastName: "Ewing",
 
-let integer = Math.random();
-function numberprint(integer) 
-{
-    for (let i =1; i<=integer; i++)
+    fullName: function() 
     {
-        console.log(" ");
-        for (let j=1;j<=integer;j++)
-        {
-            console.log(j);
-        }
+        return `${this.firstName} ${this.lastName}`
     }
+
 }
-console.log(numberprint);
